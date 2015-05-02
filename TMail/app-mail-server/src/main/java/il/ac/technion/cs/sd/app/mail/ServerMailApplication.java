7f436e7c0751cac5e503ebc0db4b83b5efb9b6e2
odedgr <theog2788@gmail.com>
2015-05-02 15:15:17 +0300

    initial data structures skeleton for server app
    just to be used as a reminder of data we need to retian

--- TMail/app-mail-server/src/main/java/il/ac/technion/cs/sd/app/mail/ServerMailApplication.java
+++ TMail/app-mail-server/src/main/java/il/ac/technion/cs/sd/app/mail/ServerMailApplication.java
@@ -1,5 +1,9 @@
 package il.ac.technion.cs.sd.app.mail;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -8,14 +12,28 @@ import java.util.UUID;
  */
 public class ServerMailApplication {
 	
+	// instance variables
+	private String myAddress = null;
+
+	// loaded / stored to independent db
+	private Map<String, List<Envelope>> mailBySender = new HashMap<String, List<Envelope>>();
+	private Map<String, List<Envelope>> mailByReceiver = new HashMap<String, List<Envelope>>();
+	
+
+	// instance methods
+	private boolean active = false;
+	
 	/**
 	 * Starts a new web server with an arbitrary name. If you want to generate a random name, look at
 	 * {@link UUID#randomUUID()}.
 	 */
 	public ServerMailApplication() {
+//		myAddress = ....
+		loadDb();
 		throw new UnsupportedOperationException("Not implemented");
 	}
 	
+
 	/**
 	 * @return the server's address; this address will be used by clients connecting to the server
 	 */
@@ -27,6 +45,9 @@ public class ServerMailApplication {
 	 * Starts the server; any previously sent mails, data and indices are loaded
 	 */
 	public void start() {
+		
+		
+		this.active = true;
 		throw new UnsupportedOperationException("Not implemented");
 	}
 	
@@ -35,6 +56,10 @@ public class ServerMailApplication {
 	 * A stopped server does not use any system resources (e.g., messengers).
 	 */
 	public void stop() {
+		
+		this.active = false;
+		saveDb();
+		
 		throw new UnsupportedOperationException("Not implemented");
 	}
 	
@@ -44,5 +69,77 @@ public class ServerMailApplication {
 	 */
 	public void clean() {
 		throw new UnsupportedOperationException("Not implemented");
+	}
+	
+	
+	////////////////
+	
+	private void addNewMail(Mail mail) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented");
+	}
+	
+	private List<Mail> getMailOfSender(String sender, int howMany) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented");
+//		List<Mail> list = new ArrayList<Mail>();
+//		
+//		return list
+	}
+	
+	private List<Mail> getMailOfReceiver(String receiver, int howMany) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented");
+//		List<Mail> list = new ArrayList<Mail>();
+//		
+//		return list
+	}
+	
+	private List<Mail> getAllMailOfClient(String client, int howMany) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented");
+//		List<Mail> list = new ArrayList<Mail>();
+//		
+//		return list
+	}
+	
+	private List<Mail> getUnreadMailOfClient(String client, int howMany) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented");
+//		List<Mail> list = new ArrayList<Mail>();
+//		
+//		return list
+	}
+	
+
+	private void loadDb() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented");
+	}
+	
+	private void saveDb() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented");
+	}
+	
+	
+	private class Envelope {
+		private Mail msg;
+		private LocalDateTime arrivalTime;
+		private boolean opened = false;
+		
+		Envelope(Mail mail) {
+			msg = mail;
+			opened = false;
+			arrivalTime = LocalDateTime.now();
+		}
+		
+		public void markRead() {
+			opened = true;
+		}
+		
+		public boolean wasRead() {
+			return opened;
+		}
 	}
 }
