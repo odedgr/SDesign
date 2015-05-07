23583d3fb355fa5651716807c842b43dcbd79f6b
odedgr <theog2788@gmail.com>
2015-05-07 18:37:02 +0300

    added client's address (un)packing into messages
    added packing the String address on the client's send method, and
    unpacking it from raw byte[] on the server's receive methods.

--- TMail/library-client/src/main/java/il/ac/technion/cs/sd/lib/ClientConnection.java
+++ TMail/library-client/src/main/java/il/ac/technion/cs/sd/lib/ClientConnection.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,7 +18,6 @@ import il.ac.technion.cs.sd.msg.MessengerFactory;
 // TODO: pick a better name that T?
 // TODO: Wrap all the serlilize and conversion in a a common class or utility func.
 // TODO: javadoc.
-// TODO: test?
 
 /**
  * 
@@ -59,15 +60,47 @@ public class ClientConnection<T extends Serializable> {
 			oos = new ObjectOutputStream(bos);
 			oos.writeObject(msg);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		try {
-			messenger.send(serverAddress, bos.toByteArray());
+			messenger.send(serverAddress, wrapWithMyAddress(bos.toByteArray()));
 		} catch (MessengerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	
+	// TODO method with Charset parameter might not be necessary. maybe only system default is enough.
+	/**
+	 * Wrap a byte[] representing an object to be sent, with this client's address byte representation, using a specific Charset.
+	 * 
+	 * @param objBytes The byte[] to be wrapped with this client's address.
+	 * @param charset The Charset to be used for encoding the address String.
+	 * @return byte[] including this client's address and the serialized (encoded) object.
+	 */
+	private byte[] wrapWithMyAddress(byte[] objBytes, Charset charset) {
+		byte[] addressBytes = messenger.getAddress().getBytes(charset);
+		int addressSizeInBytes = addressBytes.length;
+		
+		ByteBuffer buff = ByteBuffer.allocate(Integer.BYTES + addressSizeInBytes + objBytes.length);
+		buff.putInt(addressSizeInBytes)
+			.put(addressBytes)
+			.put(objBytes);
+		
+		return buff.array();
+	}
+	
+	/**
+	 * Wrap a byte[] representing an object to be sent, with this client's address byte representation.
+	 * 
+	 * The clients address, represented as a String object, is encoded using the system's default Charset encoding.
+	 * 
+	 * @param objBytes The byte[] to be wrapped with this client's address.
+	 * @return byte[] including this client's address and the serialized (encoded) object.
+	 */
+	private byte[] wrapWithMyAddress(byte[] objBytes) {
+		return wrapWithMyAddress(objBytes, Charset.defaultCharset());
 	}
 	
 	public Optional<T> receiveSingle() {
@@ -77,13 +110,14 @@ public class ClientConnection<T extends Serializable> {
 			if (!bytes.isPresent()) {
 				return Optional.empty();
 			}
-
 			ByteArrayInputStream bis = new ByteArrayInputStream(bytes.get());
 			ObjectInputStream ois;
 			ois = new ObjectInputStream(bis);
-			return (Optional<T>) ois.readObject(); // TODO: shouldn't the cast be to T, and only then wrap with Optional?
+			
+			@SuppressWarnings("unchecked")
+			Optional<T> retVal = (Optional<T>) Optional.of(ois.readObject());
+			return retVal;
 		} catch (MessengerException | ClassNotFoundException | IOException e) {
-			// TODO Auto-generated catch block
 			// TODO - What to do in case of failure?!
 			e.printStackTrace();
 		}
@@ -97,9 +131,11 @@ public class ClientConnection<T extends Serializable> {
 			ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
 			ObjectInputStream ois;
 			ois = new ObjectInputStream(bis);
-			return (T)ois.readObject();
+			
+			@SuppressWarnings("unchecked")
+			T retVal = (T)ois.readObject();
+			return retVal;
 		} catch (MessengerException | ClassNotFoundException | IOException e) {
-			// TODO Auto-generated catch block
 			// TODO - What to do in case of failure?!
 			e.printStackTrace();
 		}
--- TMail/library-server/src/main/java/il/ac/technion/cs/sd/lib/ServerConnection.java
+++ TMail/library-server/src/main/java/il/ac/technion/cs/sd/lib/ServerConnection.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Optional;
 
@@ -63,8 +65,8 @@ public class ServerConnection<T extends Serializable> {
 		}
 	}
 	
-	// TODO: how do we get the address of the client?
 	public Optional<T> receiveSingle() {
+		// TODO change return type, so both the object and the clients source address ('from') is returned.
 		Optional<byte[]> bytes;
 		try {
 			bytes = messenger.tryListen();
@@ -72,28 +74,84 @@ public class ServerConnection<T extends Serializable> {
 				return Optional.empty();
 			}
 
-			ByteArrayInputStream bis = new ByteArrayInputStream(bytes.get());
-			ObjectInputStream ois;
-			ois = new ObjectInputStream(bis);
+			ByteBuffer buff = ByteBuffer.wrap(bytes.get());
+			
+			String from = extractClientAddress(buff);
+			byte[] msgBytes = extractPayload(buff, from.getBytes().length);
+			
+			ByteArrayInputStream bis = new ByteArrayInputStream(msgBytes);
+			ObjectInputStream ois = new ObjectInputStream(bis);
+			
 			return (Optional<T>) ois.readObject(); // TODO: shouldn't the cast be to T, and only then wrap with Optional?
 		} catch (MessengerException | ClassNotFoundException | IOException e) {
-			// TODO Auto-generated catch block
 			// TODO - What to do in case of failure?!
 			e.printStackTrace();
 		}
 		return Optional.empty();
 	}
 
+	/**
+	 * Extract raw bytes (byte[]) of serialized object from raw received message.
+	 * 
+	 * @param buff ByteBuffer wrapping the raw byte[] as received from messenger.
+	 * @param offset Start point in the byte[] in the ByteBuffer, from which to extract the object's byte[]
+	 * @return byte[] containing only the original serialized (encoded) Object.
+	 */
+	private byte[] extractPayload(ByteBuffer buff, int offset) {
+		
+		int metadataSize = offset + Integer.BYTES;
+		
+		byte[] bytes = new byte[buff.capacity() - metadataSize];
+		buff.get(bytes, metadataSize, bytes.length);
+		
+		return buff.array();
+	}
+
+	/**
+	 * Re-build the client's address as a String object from raw message, using the system's default Charset.
+	 * 
+	 * @param buff ByteBuffer object wrapped around the raw byte[] of received message.
+	 * @return String object representing the sending client's address.
+	 */
+	private String extractClientAddress(ByteBuffer buff) {
+		return extractClientAddress(buff, Charset.defaultCharset());
+	}
+	
+	// TODO method with specific charset might not be necessary, maybe only system's default encoding is needed
+	/**
+	 * Re-build the client's address as a String object from raw message, using a specific Charset.
+	 * 
+	 * @param buff ByteBuffer object wrapped around the raw byte[] of received message.
+	 * @param charset Charset to be used for decoding bytes into a String object.
+	 * @return String object representing the sending client's address.
+	 */
+	private String extractClientAddress(ByteBuffer buff, Charset charset) {
+		int addressSizeInBytes = buff.getInt(0);
+		byte[] addressBytes = new byte[addressSizeInBytes]; 
+		
+		buff.get(/* output destination */    addressBytes, 
+				 /* offset in byte buffer */ Integer.BYTES, 
+				 /* amount of bytes */       addressSizeInBytes);
+		
+		return new String(addressBytes, charset);
+	}
+
 	public T receiveSingleBlocking() {
+		// TODO change return type so it contains both the object and the source client's address ('from')
 		byte[] bytes;
 		try {
 			bytes = messenger.listen();
-			ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+			
+			ByteBuffer buff = ByteBuffer.wrap(bytes);
+			
+			String from = extractClientAddress(buff);
+			byte[] msgBytes = extractPayload(buff, from.getBytes().length);
+
+			ByteArrayInputStream bis = new ByteArrayInputStream(msgBytes);
 			ObjectInputStream ois;
 			ois = new ObjectInputStream(bis);
 			return (T)ois.readObject();
 		} catch (MessengerException | ClassNotFoundException | IOException e) {
-			// TODO Auto-generated catch block
 			// TODO - What to do in case of failure?!
 			e.printStackTrace();
 		}
