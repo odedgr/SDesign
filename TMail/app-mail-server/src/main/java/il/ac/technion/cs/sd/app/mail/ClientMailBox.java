5dab07edb5695275aeac7f73903b3e4a82c64d57
odedgr <theog2788@gmail.com>
2015-05-09 15:27:10 +0300

    Initial support for correspondence retrieval
    needed to add another map in the ClientMailBox class, to get mail
    directly in O(n).
    Also extracted a common behavior of getting last N entries into a
    separate method. (DRYer!)

--- TMail/app-mail-server/src/main/java/il/ac/technion/cs/sd/app/mail/ClientMailBox.java
+++ TMail/app-mail-server/src/main/java/il/ac/technion/cs/sd/app/mail/ClientMailBox.java
@@ -19,6 +19,7 @@ public class ClientMailBox {
 	Map<Long, MailEntry> unread = new HashMap<Long, MailEntry>(); // map (Time of arrival : MailEntry)
 	List<MailEntry> sent = new ArrayList<MailEntry>(); // ordered from oldest (start) to newest (end). new mails are appended at the end
 	List<MailEntry> inbox = new ArrayList<MailEntry>(); // ordered from oldest (start) to newest (end). new mails are appended at the end
+	Map<String, List<MailEntry>> correspondece = new HashMap<String, List<MailEntry>>();
 	
 	/**
 	 * Add a new mail entry, when this client sends a new mail. Updates this client's contact list if needed.
@@ -28,6 +29,7 @@ public class ClientMailBox {
 	public void addSentMailEntry(MailEntry me) {
 		this.sent.add(me);
 		this.contacts.add(me.mail.to); // "this" client sent the mail
+		// TODO add to correspondece
 	}
 	
 	/**
@@ -39,6 +41,7 @@ public class ClientMailBox {
 		this.inbox.add(me);
 		this.unread.put(me.time, me);
 		this.contacts.add(me.mail.from); // "this" client received the mail
+		// TODO add to correspondece
 	}
 	
 	
@@ -70,49 +73,16 @@ public class ClientMailBox {
 	}
 	
 	/**
-	 * Get a list of at most N most recent MailEntry items this client sent.
-	 * 
-	 * @param n - maximal amount of MailEntry items to return.
-	 * @return List of at most N most recent MailEntry items this client sent.
-	 */
-	private List<MailEntry> getLastNSentEntries(int n) {
-		int topLimit = this.sent.size();
-		
-		if (topLimit < n) {
-			n = topLimit;
-		}
-		
-		return this.sent.subList(topLimit - n, topLimit);
-	}
-	
-	
-	/**
 	 * Get a list of at most N most recently sent mail items by this client.
 	 * 
 	 * @param n Maximal amount of mail items to be returned.
 	 * @return List of at most N most recent mail items this client sent.
 	 */
 	public List<Mail> getLastNSent(int n) {
-		return getLastNSentEntries(n)
+		return getLastEntries(this.sent, n)
 				.stream()
 				.map(me -> me.mail)
 				.collect(Collectors.toCollection(ArrayList::new));
-	}
-
-	/**
-	 * Get a list of at most N most recent MailEntry items this client received (both read and unread). Does NOT mark as read.
-	 * 
-	 * @param n - maximal amount of mail items to return.
-	 * @return List of at most N most recent mails this client received.
-	 */
-	private List<MailEntry> getLastNReceivedEntries(int n) {
-		int topLimit = this.inbox.size();
-		
-		if (topLimit < n) {
-			n = topLimit;
-		}
-		
-		return this.inbox.subList(topLimit - n, topLimit); // start inclusive, end exclusive
 	}
 	
 	/**
@@ -124,7 +94,7 @@ public class ClientMailBox {
 	public List<Mail> getLastNReceived(int n) {
 		List<Mail> lastReceived = new ArrayList<Mail>();
 
-		for (MailEntry me : getLastNReceivedEntries(n)) {
+		for (MailEntry me : getLastEntries(this.inbox, n)) {
 			readMailEntry(me);
 			lastReceived.add(me.mail);
 		}
@@ -139,8 +109,8 @@ public class ClientMailBox {
 	 * @return List of N most recent Mail items.
 	 */
 	public List<Mail> getLastNMails(int n) {
-		List<MailEntry> lastSent     = getLastNSentEntries(n),
-		                lastReceived = getLastNReceivedEntries(n);
+		List<MailEntry> lastSent     = getLastEntries(this.sent, n),
+		                lastReceived = getLastEntries(this.inbox, n);
 		
 		List<Mail> combinedList = new ArrayList<Mail>();
 		
@@ -183,4 +153,43 @@ public class ClientMailBox {
 	public List<String> getContacts() {
 		return new ArrayList<String>(this.contacts); // TODO maybe conversion to list is not needed
 	}
+
+	/**
+	 * Get a list of this client's correspondence with another client.
+	 * 
+	 * @param otherClient Address of other client, with whom correspondence is required.
+	 * @param howMany Maximal amount of returned Mail items.
+	 * @return A list of Mail items sent between this and the other client, ordered by time of arrival.
+	 */
+	public List<Mail> getCorrespondeceWith(String otherClient, int howMany) {
+		List<MailEntry> allMail = this.correspondece.get(otherClient);
+		int topLimit = allMail.size();
+		
+		if (howMany > topLimit) {
+			howMany = topLimit;
+		}
+		
+		this.correspondece.get(otherClient).subList(topLimit - howMany, topLimit);
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/**
+	 * Get a list of at most N most recent (last) MailEntry items from a given list of MailEntrys. Does NOT mark as read.
+	 * 
+	 * @param n - maximal amount of mail items to return.
+	 * @return List of at most N most recent mail entries from the list.
+	 */	
+	private List<MailEntry> getLastEntries(List<MailEntry> list, int howMany) {
+		int topLimit = list.size();
+		
+		if (topLimit < howMany) {
+			howMany = topLimit;
+		}
+		
+		return list.subList(topLimit - howMany, topLimit);
+	}
+	
 }
+
