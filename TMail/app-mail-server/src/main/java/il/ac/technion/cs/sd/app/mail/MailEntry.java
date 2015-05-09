aaea86c70f187f45d4488e022e69e05a8b65538b
odedgr <theog2788@gmail.com>
2015-05-09 15:25:20 +0300

    Added some Javadoc

--- TMail/app-mail-server/src/main/java/il/ac/technion/cs/sd/app/mail/MailEntry.java
+++ TMail/app-mail-server/src/main/java/il/ac/technion/cs/sd/app/mail/MailEntry.java
@@ -1,5 +1,9 @@
 package il.ac.technion.cs.sd.app.mail;
 
+/**
+ * Wrapper for Mail objects to be stored in a MailBox object, used in the Mail Server application.
+ * An Entry contains a single Mail, and marks its time of arrival at the server, and whether it was read or not.
+ */
 public class MailEntry {
 	public final Mail mail;
 	public final long time;
@@ -17,10 +21,18 @@ public class MailEntry {
 		this.read = read;
 	}
 	
+	/**
+	 * Mark the contained Mail object as read (e.g: returned by some client's request)
+	 */
 	public void markAsRead() {
 		this.read = true;
 	}
 	
+	/**
+	 * Check if the Mail in this entry was already read or not.
+	 * 
+	 * @return 'true' if the Mail was never read before, 'false' otherwise.
+	 */
 	public boolean isUnread() {
 		return (false == this.read);
 	}
